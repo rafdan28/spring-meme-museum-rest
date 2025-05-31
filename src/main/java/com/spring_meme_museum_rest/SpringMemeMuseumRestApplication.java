ef1e3ba0a8259e91d6c5cdbadb5bcac91b2fe0c5
raffaeledanna <rafdanna28@gmail.com>
2025-05-31 17:04:32 +0200

    fix

--- src/main/java/com/spring_meme_museum_rest/SpringMemeMuseumRestApplication.java
+++ src/main/java/com/spring_meme_museum_rest/SpringMemeMuseumRestApplication.java
@@ -2,9 +2,8 @@ package com.spring_meme_museum_rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class SpringMemeMuseumRestApplication {
 
 	public static void main(String[] args) {
