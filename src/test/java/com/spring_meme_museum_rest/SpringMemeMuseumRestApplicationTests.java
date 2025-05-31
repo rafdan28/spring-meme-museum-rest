f6dfbabc100bb85e969c2746743d2a5afa880d24
raffaeledanna <rafdanna28@gmail.com>
2025-05-31 18:20:30 +0200

    fix: aggiunge commento a metodo senza implementazione

--- src/test/java/com/spring_meme_museum_rest/SpringMemeMuseumRestApplicationTests.java
+++ src/test/java/com/spring_meme_museum_rest/SpringMemeMuseumRestApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class SpringMemeMuseumRestApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() { // add the test
 	}
 
 }
