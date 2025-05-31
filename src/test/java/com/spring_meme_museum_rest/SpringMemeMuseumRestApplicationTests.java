c27ea46eaa8431d67e5229205a13029444f576d3
raffaeledanna <rafdanna28@gmail.com>
2025-05-31 17:01:41 +0200

    First Commit

---
+++ src/test/java/com/spring_meme_museum_rest/SpringMemeMuseumRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_meme_museum_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMemeMuseumRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
