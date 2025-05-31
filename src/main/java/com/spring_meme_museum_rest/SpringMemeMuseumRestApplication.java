a822313d828395cb1975fc33185bcdba2f2ba5ac
raffaeledanna <rafdanna28@gmail.com>
2025-05-31 17:21:40 +0200

    esclude data source

--- src/main/java/com/spring_meme_museum_rest/SpringMemeMuseumRestApplication.java
+++ src/main/java/com/spring_meme_museum_rest/SpringMemeMuseumRestApplication.java
@@ -2,8 +2,9 @@ package com.spring_meme_museum_rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SpringMemeMuseumRestApplication {
 
 	public static void main(String[] args) {
