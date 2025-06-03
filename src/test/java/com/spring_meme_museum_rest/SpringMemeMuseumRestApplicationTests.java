da5f845c2d67a9e8f86c1fdcafb596ae9558afe4
raffaeledanna <rafdanna28@gmail.com>
2025-06-03 17:06:54 +0200

    disattiva il lancio dei test

--- src/test/java/com/spring_meme_museum_rest/SpringMemeMuseumRestApplicationTests.java
+++ src/test/java/com/spring_meme_museum_rest/SpringMemeMuseumRestApplicationTests.java
@@ -1,13 +1,12 @@
 package com.spring_meme_museum_rest;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringMemeMuseumRestApplicationTests {
 
-	@Test
-	void contextLoads() { // add the test
-	}
+	// @Test
+	// void contextLoads() { // add the test
+	// }
 
 }
