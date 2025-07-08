d6c694b3d3c97614e3f3ca61514b93644161750c
raffaeledanna <rafdanna28@gmail.com>
2025-07-09 00:18:40 +0200

    aggiunge permesso cors per patch

--- src/main/java/com/springmememuseumrest/config/CorsConfig.java
+++ src/main/java/com/springmememuseumrest/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOrigins("http://localhost:4200") // URL del tuo frontend
-            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
             .allowedHeaders("*")
             .allowCredentials(true);
     }
