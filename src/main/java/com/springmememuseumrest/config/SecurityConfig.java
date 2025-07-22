a5874f02f6dd1b4e2551c52c620ef847ee6846e7
raffaeledanna <rafdanna28@gmail.com>
2025-07-22 11:14:13 +0200

    rimuove import inutili

--- src/main/java/com/springmememuseumrest/config/SecurityConfig.java
+++ src/main/java/com/springmememuseumrest/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.springmememuseumrest.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
